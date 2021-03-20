285bbb857f635c4a8c6b77b4ee896a7742845fd9
KUAI <2542862169@qq.com>
2021-03-20 21:26:01 +0800

    Update cdn-hook.js
    尝试释放七牛cdn
    设置缓冲时间

--- cdn-hook.js
+++ cdn-hook.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         bili cdn change
-// @version      0.2
+// @version      0.2.1
 // @description  切换bilivideo 镜像 cdn 为原生cdn
 // @author       kuai
 // @match        https://www.bilibili.com/*
@@ -14,7 +14,9 @@
 		let nurl = url;
 		if (/^https:\/\/[a-z.-\d]*bilivideo.com/i.test(url)) {
 			let node = url.match(/(?<=^https:\/\/)[a-z.-\d]*(?=.bilivideo.com)/i)[0];
-			if (url.indexOf("cn-sh-fx-bcache") == -1) {
+			if (url.indexOf("upos-sz-mirrorkodo") !== -1) {
+				console.log(`%c test go ${node}`, "background-color:#00ffff");
+			} else if (url.indexOf("cn-sh-fx-bcache") === -1) {
 				let nNode = "cn-sh-fx-bcache-" + String(Math.floor(Math.random() * 9 + 1)).padStart(2, "0");
 				nurl = url.replace(/^https:\/\/[a-z.-\d]*bilivideo.com/i, "https://" + nNode + ".bilivideo.com");
 				console.log(`%c replace ${node} to ${nNode}`, "background-color:yellow");
@@ -28,5 +30,14 @@
 		}
 		return this.origin_open(method, nurl, async === undefined ? true : async, user, password);
 	};
+	const setBufferTime = () => {
+		if (window.dashPlayer) {
+			dashPlayer.setStableBufferTime(300);
+			console.log(`%c set buffer time`, "background-color:#ffff00");
+		} else {
+			setTimeout(setBufferTime, 1000);
+		}
+	};
+	setBufferTime();
 
 })();
