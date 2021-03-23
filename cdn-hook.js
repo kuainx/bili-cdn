aab0857953afe9fcbd65f373a00cd5ce1b4f759d
KUAI <2542862169@qq.com>
2021-03-23 18:57:54 +0800

    3.0

--- cdn-hook.js
+++ cdn-hook.js
@@ -1,42 +1,78 @@
 // ==UserScript==
 // @name         bili cdn change
-// @version      0.2.3
-// @description  切换bilivideo 镜像 cdn 为原生cdn
+// @version      0.3.0
+// @description  切换bilivideo 劣质cdn 为 优质cdn
 // @author       kuai
 // @match        https://www.bilibili.com/*
 // @grant        none
 // ==/UserScript==
+/*
+possible bad cdn
+cn-sh-cc-bcache-07
 
+ */
 (function () {
 	"use strict";
-	XMLHttpRequest.prototype.origin_open = XMLHttpRequest.prototype.open;
-	XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
-		let nurl = url;
-		if (/^https:\/\/[a-z.-\d]*bilivideo.com/i.test(url)) {
-			let node = url.match(/(?<=^https:\/\/)[a-z.-\d]*(?=.bilivideo.com)/i)[0];
-			if (url.indexOf("upos-sz-mirrorkodo") !== -1) {
-				console.log(`%c test go ${node}`, "background-color:#00ffff");
-			} else if (url.indexOf("cn-sh-fx-bcache") === -1) {
-				let nNode = "cn-sh-fx-bcache-" + String(Math.floor(Math.random() * 9 + 1)).padStart(2, "0");
-				nurl = url.replace(/^https:\/\/[a-z.-\d]*bilivideo.com/i, "https://" + nNode + ".bilivideo.com");
-				console.log(`%c replace ${node} to ${nNode}`, "background-color:yellow");
-			} else {
-				console.log(`%c go ${node}`, "background-color:#00ff00");
-			}
-		} else if (/^https:\/\/[a-z\d]*.cachenode.cn/i.test(url)) {
-			let node = url.match(/(?<=^https:\/\/)[a-z\d]*\.cachenode.cn:\d*\/[a-z.-\d]*(?=.bilivideo.com)/i)[0];
-			console.log(`%c unable to deal ${node}`, "background-color:#00ffff");
-			//无法抓取到该链接
-		}
-		setBuffer();
-		return this.origin_open(method, nurl, async === undefined ? true : async, user, password);
+	let requestedDomain = {
+		bilivideoGoodNode: new Set(),
+		bilivideoBadNode: new Set(),
+		others: new Set(),
 	};
+	window.requestedDomain = requestedDomain;
+	const goodCdn = [
+		"upos-sz-mirrorkodo",
+	];
+	for (let i = 1; i <= 10; i++) {
+		goodCdn.push("cn-sh-fx-bcache-" + String(i).padStart(2, "0"));
+	}
+	for (let i = 1; i <= 14; i++) {
+		goodCdn.push("cn-sh-cc-bcache-" + String(i).padStart(2, "0"));
+	}
+
 	const setBuffer = async () => {
 		if (window.dashPlayer) {
-			if (dashPlayer.getStableBufferTime() !== 300) {
-				dashPlayer.setStableBufferTime(300);
+			if (window.dashPlayer.getStableBufferTime() !== 300) {
+				window.dashPlayer.setStableBufferTime(300);
 				console.log(`%c set buffer time`, "background-color:#ff0000");
 			}
 		}
 	};
+	const putRequestedDomain = async (domain, from, nNode) => {
+		switch (from) {
+			case "bilivideoGoodNode":
+				console.log(`%c go ${domain}`, "background-color:#00ff00");
+				break;
+			case "bilivideoBadNode":
+				console.log(`%c replace ${domain} to ${nNode}`, "background-color:yellow");
+				break;
+			case "others":
+				let node = domain.match(/(?<=\/\/)[^\/]*(?=\/)/i);
+				if (node) {
+					domain = node[0];
+				} else {
+					console.log(domain);
+				}
+		}
+		requestedDomain[from].add(domain);
+	};
+
+	XMLHttpRequest.prototype.origin_open = XMLHttpRequest.prototype.open;
+	XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
+		let nUrl = url;
+		if (/^https:\/\/[a-z.-\d]*bilivideo.com/i.test(url)) {
+			let node = url.match(/(?<=^https:\/\/)[a-z.-\d]*(?=.bilivideo.com)/i)[0];
+			if (goodCdn.includes(node)) {
+				putRequestedDomain(node, "bilivideoGoodNode");
+			} else {
+				let nNode = goodCdn[Math.floor(Math.random() * goodCdn.length)];
+				nUrl = url.replace(/^https:\/\/[a-z.-\d]*bilivideo.com/i, "https://" + nNode + ".bilivideo.com");
+				putRequestedDomain(node, "bilivideoBadNode", nNode);
+			}
+			setBuffer();
+		} else {
+			putRequestedDomain(url, "others");
+		}
+		return this.origin_open(method, nUrl, async === undefined ? true : async, user, password);
+	};
+
 })();
